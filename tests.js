4e1e31f1f989949fad642d33feeb47b897aed1a2
Lance Hunter <lancehunter@gmail.com>
2017-11-08 14:56:40 -0600

    Added Paige's tests

--- tests.js
+++ tests.js
@@ -28,8 +28,37 @@ describe("roll function", function () {
   it("returns yet another random number", function () {
      expect(podFunction.roll(dice)).to.be.a('number');
   })
-
 })
+
+describe("set sides", function() {
+  it("sets key:value pairs in object", function() {
+    expect(podFunction.setSides(6)).to.deep.equal(
+      {
+        1: [1],
+        2: [2],
+        3: [3],
+        4: [4],
+        5: [5],
+        6: [6]
+      });
+
+ })
+})
+
+describe("get image", function() {
+      it("returns the image associated with the side", function() {
+        expect(podFunction.getImage(dice .1)).to.equal("image placeholder1");
+      })
+      it("returns the image associated with the side", function() {
+          expect(podFunction.getImage(dice .7)).to.equal(
+              "Pick a number 1-6");
+
+
+         })
+      })
+
+
+
 
 describe("2 side minimum", function () {
   it("a Dice must have at least 2 sides", function () {
